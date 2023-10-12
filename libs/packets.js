62e38cc77d6c12ba223a40e87fcae54cec1ca2c3
byHyakimaro <moderrevolution090217@gmail.com>
2023-10-12 08:28:31 -0300

    aC

---
+++ libs/packets.js
@@ -0,0 +1,3 @@
+class ManagerPackets {
+  constructor() {};
+}
--- libs/win32.js
+++ libs/win32.js
@@ -1,5 +1,6 @@
 import ffi from 'ffi-napi';
 import process from 'process';
+import { exec } from 'child_process';
 
 class ManagerWin32 {
   constructor(){
@@ -7,7 +8,7 @@ class ManagerWin32 {
       "ShellExecuteA": [
         "int32", ["int32","string","string","string","string","int"]
       ]
-    })
+    });
   };
 
   _TEXT(text) {
@@ -15,7 +16,7 @@ class ManagerWin32 {
   }
 
   shell32(command) {
-    this.Shell32.ShellExecuteA(0, "open", "powershell", command, null, 0)
+    this.Shell32.ShellExecuteA(0, "open", "powershell", command, null, 0);
   }
 
   /**
@@ -25,11 +26,49 @@ class ManagerWin32 {
   * @returns {String} - value variable after update
   */
   env(variable, value, action) {
-    action === 'update' || action === 'create' ? process.env[variable] = value : delete process.env[variable]
-    return process.env[variable]
-  }
-}
+    action === 'update' || action === 'create' ? process.env[variable] = value : delete process.env[variable];
+    return process.env[variable];
+  };
+
+  /**
+  * @params {String} - name of context regedit
+  * @params {String} - Title of context
+  * @params {String} - Icon path example: "C:\\Windows\\system32\\cmd.exe"
+  * @params {String} - command execute in background example: '"C:\Program Files\Git\git-bash.exe" "--cd=%v."'
+  * @returns {String} - result of register
+  */
+  regEditBg(nameContext, TitleContext, IconPath, command){
+    const registerPathIcon = `HKEY_CLASSES_ROOT\\directory\\background\\shell\\${nameContext}`;
+    const registerIcon = `reg add "${registerPathIcon}" /v "${TitleContext}" /t REG_SZ /d "${IconPath}" /f`;
+    
+    const registerPathCommand = `HKEY_CLASSES_ROOT\\directory\\background\\shell\\${nameContext}\\command`;
+    const registerCommand = `reg add "${registerPathCommand}" /t REG_SZ /d "${command}" /f`;
+  
+    exec(registerIcon, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Err: ${err.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Err: ${stderr}`);
+        return;
+      }
+      console.log(`Registro do Windows aberto com a chave do registro selecionada: ${registerIcon}`);
+      exec(registerCommand, (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Err: ${err.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`Err: ${stderr}`);
+          return;
+        }
+        console.log(`Registro do Windows aberto com a chave do registro selecionada: ${registerCommand}`);
+      });
+    });
+  };
+};
 
 const win = new ManagerWin32
 win.shell32('start cmd')
-console.log(win.env('MSG', 'oi', 'update'))
+win.regEditBg('ADB', 'Adb Server', 'C:\\adb-sdk\\scrcpy.exe', '"C:\\adb-sdk\\scrcpy.exe" "--cd=%v."')
